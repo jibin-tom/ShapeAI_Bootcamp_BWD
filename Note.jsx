3bd32322040c256967e906a5eb5caa1290eed272
jibin-tom <84715203+jibin-tom@users.noreply.github.com>
2021-06-06 15:50:10 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+
+  return(
+    <div className = "note">
+     <h1>Javascript & React.js Bootcamp</h1>
+     <p>An informative bootcamp conducted by Shaurya Sinha for those longing to step into the world of web development.</p>
+    </div>
+  );
+}
+
+export default Note;
