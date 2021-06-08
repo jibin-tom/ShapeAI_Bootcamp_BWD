6dc8591a69f6af17d917755c0acc1caa829fa988
jibin-tom <84715203+jibin-tom@users.noreply.github.com>
2021-06-08 18:41:14 +0530

    Text changed

--- Note.jsx
+++ Note.jsx
@@ -4,8 +4,8 @@ function Note(){
 
   return(
     <div className = "note">
-     <h1>Javascript & React.js Bootcamp</h1>
-     <p>An informative bootcamp conducted by Shaurya Sinha for those longing to step into the world of web development.</p>
+     <h1>Javascript and React.js</h1>
+     <p>This was an amazing bootcamp taken up by Shaurya Sir. We covered everything from scratch including Javascrip, React.js, HTML.</p>
     </div>
   );
 }
